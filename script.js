ae55902853b6ef85dfcbfcc6d04ae5d6cf1e47ef
Mohammad Hassieb <mohammadhassieb@gmail.com>
2023-01-22 16:20:22 +0200

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* ====== calculations ====== */
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 
@@ -5,26 +6,38 @@ buttons.forEach((item) => {
   item.onclick = () => {
     if (item.id == "clear") {
       display.innerText = "";
+
     } else if (item.id == "backspace") {
       let string = display.innerText.toString();
       display.innerText = string.substr(0, string.length - 1);
+
     } else if (display.innerText != "" && item.id == "equal") {
       display.innerText = eval(display.innerText);
+
     } else if (display.innerText == "" && item.id == "equal") {
       display.innerText = "Empty!";
       setTimeout(() => (display.innerText = ""), 2000);
+
     } else {
       display.innerText += item.id;
     }
   };
 });
 
+
+/* ======= Dark Mode ======= */
 const themeToggleBtn = document.querySelector(".theme-toggler");
 const calculator = document.querySelector(".calculator");
 const toggleIcon = document.querySelector(".toggler-icon");
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
 let isDark = true;
-themeToggleBtn.onclick = () => {
-  calculator.classList.toggle("dark");
-  themeToggleBtn.classList.toggle("active");
-  isDark = !isDark;
-};
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    calculator.classList.toggle("dark");
+    themeToggleBtn.classList.toggle("active");
+})
+
